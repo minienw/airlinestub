70c3ea5eb9d5eead45e7bdcc9c96d1000db2dfd7
playtime222 <steve.kellaway@mefitihe.com>
2022-05-04 19:44:08 +0200

    AgentPoolProfiles count lowered. Created aks_latest image.

--- deploy/build_and_run_docker.sh
+++ deploy/build_and_run_docker.sh
@@ -1,2 +1,6 @@
-docker build -t airline_stub:pub_latest .
+docker build -t airline_stub:aks_latest .
 docker-compose -p airline up
+docker tag airline_stub:aks_latest stevekellaway/airline_stub:aks_latest
+docker push stevekellaway/airline_stub:aks_latest
+
+
